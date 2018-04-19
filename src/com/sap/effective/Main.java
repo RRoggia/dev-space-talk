ca8a4563a7584e8c67d18a1aba4e53c9c9bb2eea
RRoggia <renanzr@gmail.com>
2018-04-19 17:26:52 -0300

    add book class with constructor

---
+++ src/com/sap/effective/Main.java
@@ -0,0 +1,13 @@
+package com.sap.effective;
+
+import com.sap.effective.objectsmanagement.Book;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Book book = new Book("Book", 200);
+		System.out.println(book);
+
+	}
+
+}
