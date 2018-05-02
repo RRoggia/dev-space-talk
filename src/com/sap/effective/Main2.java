9881cf82531b3124a6b97be3bfd166cb42988ac4
rroggia <renanzr@gmail.com>
2018-05-01 21:59:45 -0300

    add builder pattern

--- src/com/sap/effective/Main2.java
+++ src/com/sap/effective/Main2.java
@@ -1,16 +1,21 @@
 package com.sap.effective;
 
 import com.sap.effective.builderpattern.NutritionFacts;
+import com.sap.effective.builderpattern.NutritionFactsBuilder;
 import com.sap.effective.builderpattern.NutritionFactsJavaBean;
 
 public class Main2 {
 
 	public Main2() {
+
 		new NutritionFacts(10, 10);
 
 		NutritionFactsJavaBean nutritionFacts = new NutritionFactsJavaBean();
 		nutritionFacts.setServings(10);
 		nutritionFacts.setServingSize(10);
+
+		NutritionFactsBuilder nutritionFacts3 = new NutritionFactsBuilder.Builder(10, 10).calories(10).sodium(10)
+				.build();
 	}
 
 }
