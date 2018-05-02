794ec04b9ec4cadb2b027d94327b0b9a5a2317ba
rroggia <renanzr@gmail.com>
2018-05-01 21:46:01 -0300

    add javaBean pattern

--- src/com/sap/effective/Main2.java
+++ src/com/sap/effective/Main2.java
@@ -1,11 +1,16 @@
 package com.sap.effective;
 
 import com.sap.effective.builderpattern.NutritionFacts;
+import com.sap.effective.builderpattern.NutritionFactsJavaBean;
 
 public class Main2 {
 
 	public Main2() {
 		new NutritionFacts(10, 10);
+
+		NutritionFactsJavaBean nutritionFacts = new NutritionFactsJavaBean();
+		nutritionFacts.setServings(10);
+		nutritionFacts.setServingSize(10);
 	}
 
 }
